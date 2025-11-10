48e2f1a972cf944b6509c2e06e2580218e49ad4a
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-10 16:28:20 -0300

    Adicionando lógica copa do mundo

---
+++ logica-programacao/ano.js
@@ -0,0 +1,10 @@
+//criar um programa, para verificar o ano de nascimento
+const anoAtual = 2025;
+let idade = Number(prompt("Digite a sua idade"));
+
+console.log( "Você nasceu em : ", anoAtual - idade)
+
+//Diego, 35 anos
+//Juliana, 10 anos
+// joao, 17 anos
+
