8d5d065710c16a7ffe902aae9ae342ef8c75bffe
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-11 15:01:22 -0300

    Adicionando logica de programação

---
+++ logica-programacao/meses.js
@@ -0,0 +1,4 @@
+let anos = Number(prompt("Digite um sua idade em anos"));
+let meses = anos * 12;
+
+document.write("O total de meses referente a sua idade é: " + meses);
---
+++ logica-programacao/quarto.js
@@ -0,0 +1,5 @@
+let horas = Number(prompt("Digite um horario"));
+let minutos = horas * 60
+document.write("O total em minutos é: " + minutos);
+//Algoritmo: Converter horas em minutos
+
