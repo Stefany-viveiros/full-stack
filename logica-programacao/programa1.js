57b8bb615ef761fc22fbff2c78cd5e0d3e59fa07
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-11 16:20:22 -0300

    Adicionando lógica

---
+++ logica-programacao/programa1.js
@@ -0,0 +1,23 @@
+//Vamos criar um programa, que o usuário executa uma função
+// e dentro dessa função existe uma frase. Pode pular linhas
+
+function logicaProgramacao() {
+    console.log("Bem vindo ao curso de Logica de Programação")
+}
+function programacao(programacao){
+    document.write(programacao);
+
+}
+function pularLinha () {
+    document.write("<br>")
+}
+
+logicaProgramacao();
+programacao("PHP");
+pularLinha();
+pularLinha();
+programacao("Java");
+pularLinha();
+pularLinha();
+programacao("JavaScript");
+pularLinha();
---
+++ logica-programacao/programa2.js
@@ -0,0 +1,6 @@
+
+function mostraDobro(valor){
+    document.write("O dobro de ", valor, " é ", valor * 2)
+}
+let numero= Number (prompt("Digite um número:"))
+mostraDobro(numero);
