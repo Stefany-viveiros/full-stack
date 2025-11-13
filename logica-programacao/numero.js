43da6ab3ce05cdf824f130e4b69f3e5573d1492d
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-13 14:16:06 -0300

    Adicionando if, else

---
+++ logica-programacao/numero.js
@@ -0,0 +1,8 @@
+let numero = Number(prompt("digite um número"));
+if(numero >0) {
+    alert("O numero é positivo");
+}else if (numero <0) {
+    alert("o número é negativo");
+}else {
+    alert("o número é igual a zero")
+}
