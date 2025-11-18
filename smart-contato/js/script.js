c3eebafcf59358eeaeaa39e7d81a39b4443f399a
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-18 15:06:18 -0300

    Adicionando formulario

--- smart-contato/js/script.js
+++ smart-contato/js/script.js
@@ -1,11 +1,31 @@
+const form= document.querySelector("form")
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
+const inputTel = document.getElementById("telefone")
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+
+    if (inputNome.value == "" || inputEmail.value == "" || inputTel.value== "") {
+        alert("Digite o nome");
+        return false;
+    }
+        
+    console.log("Nome", inputNome.value);
+    console.log("Email", inputEmail.value);
+    console.log("Tel", inputTel.value);
+})
 // inputEmail
 // inputTel
 
 
-function btnCadastrar(event) {
-    event.preventDefault();
+/*function btnCadastrar(event) {
+    event.preventDefault(); **evita o comportamento padrão
     console.log(inputNome.value);
     console.log(inputEmail.value);
-}
+}*/
+ 
+
+/* validação do formulário
+se o input for vazio, adicionar uma mensagem */
