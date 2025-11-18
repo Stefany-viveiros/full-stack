d6e418d9f36c56d213f0d3a63872d0c1c83772eb
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-18 16:20:32 -0300

    Adicionando lista na tela

--- smart-contato/js/script.js
+++ smart-contato/js/script.js
@@ -2,23 +2,43 @@ const form= document.querySelector("form")
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTel = document.getElementById("telefone")
+const lista = document.querySelector(".lista")
+
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
 
 
+
     if (inputNome.value == "" || inputEmail.value == "" || inputTel.value== "") {
-        alert("Digite o nome");
+        alert("Preencha todos os campos");
+        alert("Campo(a) vazio, preencha os campos")
         return false;
     }
         
     console.log("Nome", inputNome.value);
     console.log("Email", inputEmail.value);
     console.log("Tel", inputTel.value);
-})
+
+
+const li = document.createElement("li")
+
+
+li.innerHTML = `
+    <span class = "contato-nome">${inputNome.value} </span>
+    <span class = "contato-email">${inputEmail.value} </span>
+    <span class = "contato-telefone">${inputTel.value} </span>
+    `;
+// para aparecer na tela a lista que a pessoa digitar
+lista.appendChild(li)
+
+
+// Para limpar o que está escrito, para resetar
+form.reset()
+})    
+
 // inputEmail
 // inputTel
-
 
 /*function btnCadastrar(event) {
     event.preventDefault(); **evita o comportamento padrão
