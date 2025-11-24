3cadd0dfa24772a63681361092208115ccd72c1b
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-24 15:56:46 -0300

    Adicionando primeiro contato com node

---
+++ node-aula01/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+//Variavel comos dados produtos
+const produtos = [
+    {id: 1, nome: "Produto A", preco: 100 },
+    {id: 2, nome: "Produto B", preco: 150 },
+    {id: 3, nome: "Produto C", preco: 200 },
+];
+// Criar função 
+// Rota principal
+app.get('/', (req, res) => {
+    res.send('Bem Vindo ao servidor Node.js ');
+});
+
+
+//Rota de produto
+
+app.get('/produtos', (req, res) => {
+    res.send(produtos);
+})
+// Executando o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+});
