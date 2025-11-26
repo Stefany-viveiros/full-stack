61c94c22d4e06d5ec2d117606e8cc94f59d31a1f
Stefany-viveiros <viveirosstefany@gmail.com>
2025-11-26 15:03:30 -0300

    Adicionando nova pasta de node

---
+++ node-people/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+// mock
+const nomes = [
+  { id: 1, nome: "Fernanda", idade: "18" },
+  { id: 2, nome: "Caio", idade: "23" },
+  { id: 3, nome: "Pedro", idade: "56" },
+  { id: 4, nome: "Samuel", idade: "45" },
+  { id: 5, nome: "Doris", idade: "33" },
+];
+
+
+//Rota Teste
+app.get("/teste", (req,res) => {
+    res.send("API nodePeople está funcionando");
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
+});
+
+//Buscando nomes
+
+app.get("/nomes", (req,res) => {
+    res.send(nomes)
+})
